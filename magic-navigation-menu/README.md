87a3e60b47c4199691e3862443bda8f1f7205d05
ji-seung-ryu <8514199@gmail.com>
2022-05-14 05:53:53 +0000

    first

---
+++ magic-navigation-menu/README.md
@@ -0,0 +1,10 @@
+![image](https://user-images.githubusercontent.com/87757931/148624398-25599c50-3d08-4f3c-ba41-508c9075f3f0.png)
+
+[copied](https://www.youtube.com/watch?v=ArTVfdHOB-M)
+
+위 영상을 보고 코드를 작성했습니다. 코드 자체를 베끼기보다는 코드가 가진 의미를 생각해보았습니다. 
+
+box-shadow, nth-child 등과 같은 표현에 이해도가 높아졌습니다.
+
+기존의 영상에서는 하드코딩한 부분이 많아 메뉴바의 크기가 바뀜에 따라 초록색 indicator의 위치가 바뀌는 문제가 있습니다. 
+메뉴바의 크기가 바뀌어도 문제되지 않는 반응형 메뉴바를 만들었습니다. 
---
+++ wavy_circle/script.js
@@ -0,0 +1,29 @@
+let constrain = 70;
+let mouseOverContainer = document.getElementsByTagName("body");
+let ex1Layer = document.getElementById("loader");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+document.addEventListener("mousemove", function(e) {
+
+	
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer, position);
+  });
+}
+									)
